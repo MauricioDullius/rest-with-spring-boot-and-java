78e8e6a6c54418f8ded16bb98a44740ee00824f0
Mauricio DUllius <mauriciodullius7@gmail.com>
2025-02-24 11:28:06 -0300

    Refactor: Updated to align with modern technologies and best practices

---
+++ src/test/java/br/com/md/config/TestConfigs.java
@@ -0,0 +1,12 @@
+package br.com.md.config;
+
+public interface TestConfigs {
+    int SERVER_PORT = 8888;
+
+    String HEADER_PARAM_AUTHORIZATION = "Authorization";
+    String HEADER_PARAM_ORIGIN = "Origin";
+
+    String ORIGIN_MD = "https://www.md.com.br";
+    String ORIGIN_MD2 = "https://www.md2.com.br";
+    String ORIGIN_LOCAL = "http://localhost:8080";
+}
